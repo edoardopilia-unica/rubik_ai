9c47effadf75857fd6f1806bd887102239496b06
Edoardo Pilia <e.pilia@zcscompany.com>
2025-11-20 09:41:15 +0100

    improved speed by reducing unnecessary lists' search and by using sets

--- bfs.py
+++ bfs.py
@@ -1,5 +1,9 @@
 import rubik_ai as rb
 from rubik_ai import G,R,O,Y,B,W
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import itertools
+from collections import deque
+import time
 
 faces_data = [
         rb.face(rb.create_matrix(B,R,R,B,R,R,B,R,R)), # Red
@@ -22,52 +26,90 @@ working = [
 
 
 
-def execute_function_set(node, visited):
-    for p1 in [False, True]:
-        for p2 in [False, True]:
-            rrc = node.rotate_red_column(p1,p2)
-            if rrc not in visited: visited.append(rrc)
-            rrr = node.rotate_red_row (p1, p2)
-            if rrr not in visited: visited.append(rrr)
-            rf = node.rotate_face(p1,p2)
-            if rf not in visited: visited.append(rf)
+
+
+def execute_function_set(node):
+    new_nodes = set()
+    boolean_pairs = list(itertools.product([False, True], repeat=2))
+    futures = []
+    # Creiamo l'executor UNA volta sola fuori dal loop
+    with ThreadPoolExecutor(max_workers=12) as executor:
+        for p1, p2 in boolean_pairs:
+            # Submit accetta: (nome_funzione, argomento1, argomento2)
+            # Non eseguiamo la funzione qui, passiamo il riferimento!
+            futures.append(executor.submit(node.rotate_red_column, p1, p2))
+            futures.append(executor.submit(node.rotate_red_row, p1, p2))
+            futures.append(executor.submit(node.rotate_face, p1, p2))
+            
+        # Raccogliamo i risultati man mano che finiscono
+        for future in as_completed(futures):
+            result = future.result() # Qui otteniamo il nodo ruotato
+            new_nodes.add(result)
+
+    return new_nodes
+
+def elaborate(queue, iteration, debug=False):
+    expanded_list = []
+    target_iteration = 0
+    iteration = 0
+    while queue:
+            current = queue.popleft()
+
+            if current not in expanded_list: 
+                new_nodes = execute_function_set(current)
+                for node in new_nodes:
+                    if node not in queue: queue.append(node)
+                expanded_list.append(current)
+                if current == rb.target:
+                    rb.print_cube_state(current, "")
+                    return expanded_list, iteration
+                print(f"Insertion in expanded n.{iteration}/{len(queue)}")
+                target_iteration+=1
+            #if rb.target in queue and debug: 
+            #    print(f"Target individuato in lista: {target_iteration}")
+                
+            iteration+=1
 
 def main():
 
     faces_data = [
-        rb.face(rb.create_matrix(B,B,B,R,R,R,B,B,B)), # Red
+        rb.face(rb.create_matrix(B,B,B,R,R,R,B,B,B)), # 0: Red
         rb.face(rb.create_matrix(R,R,R,G,G,G,R,R,R)), # 1: Green
         rb.face(rb.create_matrix(Y,Y,Y,Y,Y,Y,Y,Y,Y)), # 2: Yellow
         rb.face(rb.create_matrix(O,O,O,B,B,B,O,O,O)), # 3: Blue
-        rb.face(rb.create_matrix(W,W,W,W,W,W,W,W,W)), # White
+        rb.face(rb.create_matrix(W,W,W,W,W,W,W,W,W)), # 4: White
         rb.face(rb.create_matrix(G,G,G,O,O,O,G,G,G))  # 5: Orange
     ]
 
-    my_cube = rb.cube(faces_data)
-    my_cube = my_cube.rotate_face(True, True)
-    visited_list = [my_cube]
-    expanded_list = []
-    execute_function_set(my_cube, visited_list)
-    expanded_list.append(my_cube)
-    index = 1
-    iteration = 0
-    for visited in visited_list:
-        if visited == rb.target:
-            print("Target ottenuto")
-            expanded_list.append(visited)
-            break
-        if visited not in expanded_list:
-            execute_function_set(visited, visited_list)
-            expanded_list.append(visited)
-            print(f"Insertion in expanded n.{iteration}/{len(visited_list)}")
-            index += 1
-        if rb.target in visited_list:
-            print("Target individuato in lista")
-        iteration+=1
+    start_time = time.time()
+    
 
-    print("Lista nodi espansi")
+    my_cube = rb.cube(faces_data)
+    my_cube = my_cube.rotate_face(False, False)
+    my_cube = my_cube.rotate_red_column(True, False)
+    my_cube = my_cube.rotate_red_row(False, False)
+    #my_cube = my_cube.rotate_red_column(True, False)
+    #my_cube = my_cube.rotate_red_column(False, True)
+    #my_cube = my_cube.rotate_face(True, False)
+    rb.print_cube_state(rb.target, "")
+    debug = True
+    
+       
+
+    iteration = 0
+    queue = deque([my_cube])
+
+    expanded_list, iteration = elaborate(queue, iteration, debug)
+    
+    elab_time = time.time()
+
+
+    print(f"Lista nodi espansi: n. iterazioni: {iteration}")
     for node in expanded_list:
-        rb.print_cube_state(node, expanded_list.index(node))
+        rb.print_cube_state(node, "")
+    
+    print(f"Tempo di elaborazione: {(elab_time-start_time)}. s --- N° nodi espansi: {iteration}")
+    
 
 if __name__=="__main__":
     main()
